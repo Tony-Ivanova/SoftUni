6d0ffd1b3fe84b342f167b8088ed498e27865e42
Tony <tony.ivanova86@gmail.com>
2022-03-28 17:50:02 +0300

    Deleting duplicating files

--- C# Courses/04.01. C# Web Basics/01. Exams/16 Jun 2019/SulsApp/Data/DatabaseConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SulsApp.Data
-{
-    public class DatabaseConfiguration
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=SULS;Trusted_Connection=True;Integrated Security=True;";
-    }
-}
--- C# Courses/04.01. C# Web Basics/01. Exams/18 Dec 2018/MUSACA/ViewModels/User/LoginInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MUSACA.ViewModels.User
-{
-    public class LoginInputModel
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-    }
-}
