25c336dcf48051d3d5fc4e5da5aba06ee88ee9f5
Tony <tony.ivanova86@gmail.com>
2022-04-12 19:41:29 +0300

    Proper links for header, footer and aside

---
+++ JS Courses/03.02. React/01. Origami/origami/src/utils/navigation.js
@@ -0,0 +1,29 @@
+const getNavigation = (userId) => {
+    const links = [
+        {
+            title: "Publications",
+            link: '/'
+        },
+        {
+            title: "Share your thoughts",
+            link: '/share'
+        },
+        {
+            title: "Profile",
+            link: `/profile/${userId}`
+        },
+        {
+            title: "Login",
+            link: '/login'
+        },
+        {
+            title: "Register",
+            link: '/register'
+        }
+    ]
+
+
+    return links;
+}
+
+export default getNavigation;
